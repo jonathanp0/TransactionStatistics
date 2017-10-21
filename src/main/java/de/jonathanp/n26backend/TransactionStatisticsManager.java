7b440e29b5d2bc45a97c830adaec828cf3674eb8
Jonathan Pilborough <jonathan@pilborough.co.uk>
2017-10-21 21:36:44 +0200

    Efficency improvements for cleanup

--- src/main/java/de/jonathanp/n26backend/TransactionStatisticsManager.java
+++ src/main/java/de/jonathanp/n26backend/TransactionStatisticsManager.java
@@ -6,8 +6,7 @@ public class TransactionStatisticsManager {
 
     public TransactionStatisticsManager()
     {
-        dataBuffer = new CircularBuffer<>(BUFFER_SIZE);
-        cumulative = new Statistics();
+        resetData();
     }
 
     public boolean addTransaction(Transaction transaction, long currentTime)
@@ -33,6 +32,7 @@ public class TransactionStatisticsManager {
     public Statistics getCumulativeStatistics(long currentTime)
     {
         Statistics statsCopy;
+
         synchronized (this)
         {
             cleanUp(currentTime);
@@ -47,16 +47,30 @@ public class TransactionStatisticsManager {
      */
     private void cleanUp(long currentTime)
     {
-        if (currentTime < lastUpdate)
-            throw new IllegalArgumentException("Time cannot go backwards");
-
+        //No time advance, no need to do anything
         if(currentTime == lastUpdate)
             return;
 
-        ListIterator<Statistics> iterator = dataBuffer.iterator(timestampBucket(lastUpdate), timestampBucket(currentTime));
+        if (currentTime < lastUpdate)
+            throw new IllegalArgumentException("Time cannot go backwards");
+
+        //If more than 60 seconds has gone by, erase everything
+        long timeDifference = currentTime - lastUpdate;
+        if(timeDifference >= BUFFER_SIZE) {
+            resetData();
+            lastUpdate = currentTime;
+            maxReset = false;
+            minReset = false;
+            return;
+        }
+
+        System.out.println("Cleaning from " + timestampBucket(lastUpdate + 1) + " " +  timestampBucket(currentTime));
+
+        ListIterator<Statistics> iterator = dataBuffer.iterator(timestampBucket(lastUpdate + 1), timestampBucket(currentTime));
 
         while(iterator.hasNext())
         {
+            System.out.println("Processing " + (iterator.nextIndex()));
             Statistics milliStats = iterator.next();
             cumulative.subtract(milliStats);
             if (milliStats.getMax() == cumulative.getMax())
@@ -65,6 +79,15 @@ public class TransactionStatisticsManager {
                 minReset = true;
             iterator.set(new Statistics());
         }
+
+        System.out.println("Cleanup complete");
+        lastUpdate = currentTime;
+    }
+
+    private void resetData()
+    {
+        dataBuffer = new CircularBuffer<>(Statistics.class, BUFFER_SIZE);
+        cumulative = new Statistics();
     }
 
     private int timestampBucket(long timestamp)
