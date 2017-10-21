26dd7dc3408856674e47b11927f91dee34970948
Jonathan Pilborough <jonathan@pilborough.co.uk>
2017-10-21 18:57:15 +0200

    Add JSON Mapping Objects

---
+++ src/main/java/de/jonathanp/n26backend/Statistics.java
@@ -0,0 +1,50 @@
+package de.jonathanp.n26backend;
+
+public class Statistics {
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    public void setAvg(double avg) {
+        this.avg = avg;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+    public double getCount() {
+        return count;
+    }
+
+    public void setCount(double count) {
+        this.count = count;
+    }
+
+    private double sum;
+    private double avg;
+    private double max;
+    private double min;
+    private double count;
+}
---
+++ src/main/java/de/jonathanp/n26backend/Transaction.java
@@ -0,0 +1,23 @@
+package de.jonathanp.n26backend;
+
+public class Transaction {
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    private double amount;
+    private long timestamp;
+}
